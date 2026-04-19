84b9fd9ba1314efa530d837920f54ac789139fbe
selvababu93 <selvababu93@gmail.com>
2026-04-19 15:34:10 +0400

    upload song page work

--- client/lib/features/home/view/pages/upload_song_page.dart
+++ client/lib/features/home/view/pages/upload_song_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/custom_field.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
@@ -9,86 +12,122 @@ class UploadSongPage extends ConsumerStatefulWidget {
   const UploadSongPage({super.key});
 
   @override
-  ConsumerState createState() => _UploadSongPageState();
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _UploadSongPageState();
+  }
 }
 
 class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   final _songNameController = TextEditingController();
-  final _artistNameController = TextEditingController();
-  var _selectedColor = Pallete.cardColor;
+  final _artistController = TextEditingController();
+  var selectedColor = Pallete.cardColor;
+  File? selectedPicture;
+  File? selectedAudio;
 
   @override
   void dispose() {
     _songNameController.dispose();
-    _artistNameController.dispose();
+    _artistController.dispose();
     super.dispose();
   }
 
+  void selectSong() async {
+    final pickedSong = pickAudio();
+  }
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        selectedPicture = pickedImage;
+      });
+    }
+  }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Upload Song'),
+        title: const Text("Upload a Song"),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.check))],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              const DottedBorder(
-                options: RoundedRectDottedBorderOptions(
-                  radius: Radius.circular(10),
-                  strokeCap: StrokeCap.round,
-                  color: Pallete.borderColor,
-                  dashPattern: [14, 4],
-                ),
-                child: SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.folder_open),
-                      SizedBox(
-                        height: 15,
+              GestureDetector(
+                onTap: () {
+                  selectImage();
+                },
+                child: selectedPicture != null
+                    ? SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadiusGeometry.circular(10),
+                          child: Image.file(
+                            selectedPicture!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : const DottedBorder(
+                        options: RoundedRectDottedBorderOptions(
+                          radius: Radius.circular(10),
+                          color: Pallete.borderColor,
+                          dashPattern: [10, 5],
+                          strokeCap: StrokeCap.round,
+                        ),
+                        child: SizedBox(
+                          height: 150,
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Text(
+                                "Upload a Thumbnail",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      Text(
-                        'Select the thumbnail for your song',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
               ),
               const SizedBox(
-                height: 40,
+                height: 30,
               ),
 
               CustomField(
-                hintText: "Pick Song",
+                hintText: "Pick a Song",
                 controller: null,
                 readOnly: true,
                 onTap: () {},
               ),
-
               const SizedBox(
                 height: 20,
               ),
-              CustomField(hintText: 'Artist', controller: _artistNameController),
+              CustomField(hintText: "Artist", controller: _artistController),
               const SizedBox(
                 height: 20,
               ),
-              CustomField(hintText: 'Song name', controller: _songNameController),
-
+              CustomField(hintText: "Song", controller: _songNameController),
               const SizedBox(
                 height: 20,
               ),
-
               ColorPicker(
-                color: _selectedColor,
+                pickersEnabled: const {ColorPickerType.wheel: true},
+                color: selectedColor,
                 onColorChanged: (Color color) {
                   setState(() {
-                    _selectedColor = color;
+                    selectedColor = color;
                   });
                 },
               ),
--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:client/core/providers/current_user_notifier.dart';
 import 'package:client/core/theme/theme.dart';
 import 'package:client/features/auth/view/pages/login_page.dart';
 import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
+import 'package:client/features/home/view/pages/upload_song_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       title: 'Music App',
       theme: AppTheme.darkThemeMode,
-      home: LoginPage(), //currentuser  const LoginPage() //: const UploadSongPage(),
+      home: currentuser == null ? const LoginPage() : const UploadSongPage(),
     );
   }
 }
